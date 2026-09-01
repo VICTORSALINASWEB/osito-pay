6fd0dffb7f7762b9c9c91a0de34dade561cfe6a6
VICTORSALINASWEB <130807354+VICTORSALINASWEB@users.noreply.github.com>
2026-09-01 09:59:20 -0500

    Add service worker support to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { importProvidersFrom } from '@angular/core';
 import { RouteReuseStrategy, provideRouter, withComponentInputBinding, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage-angular';
-
+ import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 import { registerLocaleData } from '@angular/common';
 import localeEsPe from '@angular/common/locales/es-PE';
-import { LOCALE_ID } from '@angular/core';
+import { LOCALE_ID, isDevMode } from '@angular/core';
 
 registerLocaleData(localeEsPe)
 
@@ -20,6 +20,7 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules), withComponentInputBinding()),
     importProvidersFrom(IonicStorageModule.forRoot()),
-    { provide: LOCALE_ID, useValue: 'es-PE' }
+    { provide: LOCALE_ID, useValue: 'es-PE' },
+    provideServiceWorker('ngsw-worker.js', { enabled: !isDevMode(), registrationStrategy: 'registerWhenStable:30000' })
   ],
 });
